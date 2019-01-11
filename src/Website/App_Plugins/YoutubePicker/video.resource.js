66a171bad3580b35273b774ad5874e2c4c6e6f70
Ian Houghton <sales@redmorello.com>
2019-01-11 16:44:25 +0000

    Initial commit

---
+++ src/Website/App_Plugins/YoutubePicker/video.resource.js
@@ -0,0 +1,18 @@
+﻿// adds the resource to umbraco.resources module:
+angular.module('umbraco.resources').factory('videoResource', 
+    function($q, $http, umbRequestHelper) {
+        // the factory object returned
+        return {
+            getAll: function (apikey, channelId) {
+                return umbRequestHelper.resourcePromise(
+                    $http.get("backoffice/YoutubePicker/YoutubeApi/GetAll/?apikey=" + apikey + "&channelId=" + channelId),
+                    "Failed to retrieve the list of Videos");
+            },
+            getAllPlaylists: function (apikey, channelId) {
+                return umbRequestHelper.resourcePromise(
+                    $http.get("backoffice/YoutubePicker/YoutubeApi/GetAllPlaylists/?apikey=" + apikey + "&channelId=" + channelId),
+                    "Failed to retrieve the list of Playlists");
+            }
+        };
+    }
+); 
---
+++ src/Website/App_Plugins/YoutubePicker/youtubepicker.overlay.controller.js
@@ -0,0 +1,55 @@
+﻿angular.module("umbraco")
+    .controller("Our.Umbraco.YoutubePicker.Overlay", function ($scope, videoResource) {
+
+        $scope.onChange = function () {
+            console.log($scope.model);
+
+            if ($scope.model.selectedOption === 'Videos') {
+                $scope.model.showPlaylists = false;
+                videoResource.getAll($scope.model.apikey, $scope.model.channelId).then(function (response) {
+                    $scope.model.videos = response;
+                    console.log($scope.model.videos);
+                    $scope.model.showVideos = true;
+                    $scope.model.type = 'video';
+                });
+            } else if ($scope.model.selectedOption === 'Playlists') {
+                $scope.model.showVideos = false;
+                videoResource.getAllPlaylists($scope.model.apikey, $scope.model.channelId).then(function (response) {
+                    $scope.model.playlists = response;
+                    //console.log($scope.model.playlists);
+                    $scope.model.showPlaylists = true;
+                    $scope.model.type = 'playlist';
+                });
+            }
+        };
+
+        $scope.selectId = function(id) {
+            $scope.model.selectedId = id;
+            $scope.model.hideSubmitButton = false;
+        };
+
+        $scope.clickItem = function (item, items, $index) {
+
+            console.log(items);
+
+            for (i = 0; i < items.length; i++) {
+                if (i != $index) {
+                    var it = items[i];
+                    if (it.selected) {
+                        it.selected = false;
+                    }
+                }
+            }
+            if (item.selected) {
+                item.selected = false;
+                $scope.model.selectedId = null;
+                $scope.model.hideSubmitButton = true;
+            } else {
+                item.selected = true;
+                $scope.model.selectedId = item.id;
+                $scope.model.hideSubmitButton = false;
+            }
+            
+            return item;
+        };
+    });
